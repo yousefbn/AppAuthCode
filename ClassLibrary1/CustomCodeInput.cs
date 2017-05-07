f5e9724ed648720ddd51c7eb888e87b786f37b08
yousefbn <yousef199400@hotmail.com>
2017-05-07 09:03:52 +0300

    Add files via upload

---
+++ ClassLibrary1/CustomCodeInput.cs
@@ -0,0 +1,447 @@
+﻿using System;
+using Android.Views;
+using Android.Animation;
+using Android.Graphics;
+using Java.Lang;
+using Android.Util;
+using Android.Content;
+using Android.Content.Res;
+using Android.Runtime;
+using Android.Views.InputMethods;
+using System.Linq;
+using Android.Views.Animations;
+using Java.Util.Regex;
+using Android.OS;
+
+namespace ClassLibrary1
+{
+    public class CustomCodeInput : View
+    {
+
+        //static const
+        private const int DEFAULT_CODES = 6;
+        //static const
+        private const string KEYCODE = "KEYCODE_";
+        public Character chracter;
+        private FixedStack<Character> characters;
+        private static Java.Util.Regex.Pattern KEYCODE_PATTERN = Java.Util.Regex.Pattern.Compile(KEYCODE + "(\\w" + ")");
+        private Underline[] underlines;
+        private Paint underlinePaint;
+        private Paint underlineSelectedPaint;
+        private Paint textPaint;
+        private Paint hintPaint;
+        private ValueAnimator reductionAnimator;
+        private ValueAnimator hintYAnimator;
+        private ValueAnimator hintSizeAnimator;
+        private float underlineReduction;
+        private float underlineStrokeWidth;
+        private float underlineWidth;
+        private float reduction;
+        private float textSize;
+        private float textMarginBottom;
+        private float hintX;
+        private float hintNormalSize;
+        private float hintSmallSize;
+        private float hintMarginBottom;
+        private float hintActualMarginBottom;
+        private float viewHeight;
+        private long animationDuration;
+        private int height;
+        private int underlineAmount;
+        private int underlineColor;
+        private int underlineSelectedColor;
+        private int hintColor;
+        private int textColor;
+        private bool underlined = true;
+        private string hintText;
+        private int mInputType;
+        private codeReadyListener listener;
+
+        protected CustomCodeInput(IntPtr javaReference, JniHandleOwnership transfer)
+            : base (javaReference, transfer)
+        {
+
+        }
+        //Constructors
+        public CustomCodeInput(Context context) : base(context)
+        {
+            init(null);
+            this.listener = null;
+        }
+        public CustomCodeInput(Context context, IAttributeSet attributeset) : base(context, attributeset)
+        {
+            init(attributeset);
+            this.listener = null;
+        }
+        public CustomCodeInput(Context context, IAttributeSet attributeset, int defStyledAttrs) : base(context, attributeset, defStyledAttrs)
+        {
+            init(attributeset);
+            this.listener = null;
+        }
+        //End of Constructors 
+        public void setCodeReadyListener(codeReadyListener listener)
+        {
+            this.listener = listener;
+        }
+        private void init(IAttributeSet at)
+        {
+            initDefaultAttributes();
+            initCustomAttributes(at);
+            initDataStructures();
+            initPaint();
+            initAnimator();
+            initViewOptions();
+        }
+        public interface codeReadyListener
+        {
+            // These methods are the different events and
+            // need to pass relevant arguments related to the event triggered
+            void onCodeReady(Character[] code);
+        }
+        private void initDefaultAttributes()
+        {
+            hintMarginBottom = Context.Resources.GetDimension(Resource.Dimension.hint_margin_bottom);
+            underlineStrokeWidth = Context.Resources.GetDimension(Resource.Dimension.underline_stroke_width);
+            underlineWidth = Context.Resources.GetDimension(Resource.Dimension.underline_width);
+            underlineReduction = Context.Resources.GetDimension(Resource.Dimension.section_reduction);
+            textSize = Context.Resources.GetDimension(Resource.Dimension.text_size);
+            textMarginBottom = Context.Resources.GetDimension(Resource.Dimension.text_margin_bottom);
+
+#pragma warning disable CS0618 // Type or member is obsolete
+            underlineColor = Context.Resources.GetColor(Resource.Color.underline_default_color);
+#pragma warning restore CS0618 // Type or member is obsolete
+#pragma warning restore CS0618 // Type or member is obsolete
+#pragma warning disable CS0618 // Type or member is obsolete
+            underlineSelectedColor = Context.Resources.GetColor(Resource.Color.underline_selected_color);
+#pragma warning restore CS0618 // Type or member is obsolete
+#pragma warning disable CS0618 // Type or member is obsolete
+            hintColor = Context.Resources.GetColor(Resource.Color.hintColor);
+#pragma warning restore CS0618 // Type or member is obsolete
+#pragma warning disable CS0618 // Type or member is obsolete
+            textColor = Context.Resources.GetColor(Resource.Color.textColor);
+#pragma warning restore CS0618 // Type or member is obsolete
+#pragma warning restore CS0618 // Type or member is obsolete
+            hintMarginBottom = Context.Resources.GetDimension(Resource.Color.underline_default_color);
+            hintNormalSize = Context.Resources.GetDimension(Resource.Dimension.hint_size);
+            hintSmallSize = Context.Resources.GetDimension(Resource.Dimension.hint_small_size);
+            animationDuration = Context.Resources.GetInteger(Resource.Color.underline_default_color);
+            viewHeight = Context.Resources.GetDimension(Resource.Dimension.view_height);
+            hintX = 0;
+            hintActualMarginBottom = 0;
+            underlineAmount = DEFAULT_CODES;
+            reduction = 0.0F;
+            hintX = 0;
+            hintActualMarginBottom = 0;
+            underlineAmount = DEFAULT_CODES;
+            reduction = 0.0F;
+        }
+        private void initCustomAttributes(IAttributeSet attributeset)
+        {
+            TypedArray attributes = Context.ObtainStyledAttributes(attributeset, Resource.Styleable.core_area);
+            underlineColor = attributes.GetColor(Resource.Styleable.core_area_underline_color, underlineColor);
+            underlineSelectedColor = attributes.GetColor(Resource.Styleable.core_area_underline_selected_color, underlineSelectedColor);
+            hintColor = attributes.GetColor(Resource.Styleable.core_area_hint_color, hintColor);
+            hintText = attributes.GetString(Resource.Styleable.core_area_hint_text);
+            underlineAmount = attributes.GetInt(Resource.Styleable.core_area_codes, underlineAmount);
+            textColor = attributes.GetInt(Resource.Styleable.core_area_text_color, textColor);
+            attributes.Recycle();
+        }
+        private void initDataStructures()
+        {
+            underlines = new Underline[underlineAmount];
+            characters = new FixedStack<Character>();
+            characters.setMaxSize(underlineAmount);
+
+        }
+        public void initPaint()
+        {
+            underlinePaint = new Paint();
+            // underlinePaint.C
+#pragma warning disable CS0618 // Type or member is obsolete
+            underlinePaint.Color=Resources.GetColor(underlineColor);
+#pragma warning restore CS0618 // Type or member is obsolete
+            underlinePaint.StrokeWidth= underlineStrokeWidth;
+            underlinePaint.SetStyle(Paint.Style.Stroke);
+            underlineSelectedPaint = new Paint();
+#pragma warning disable CS0618 // Type or member is obsolete
+            underlineSelectedPaint.Color = Resources.GetColor(underlineSelectedColor);
+#pragma warning restore CS0618 // Type or member is obsolete
+            underlineSelectedPaint.StrokeWidth=(underlineStrokeWidth);
+            underlineSelectedPaint.SetStyle(Paint.Style.Stroke);
+            textPaint = new Paint();
+            textPaint.TextSize=textSize;
+#pragma warning disable CS0618 // Type or member is obsolete
+            textPaint.Color=Resources.GetColor(textColor);
+#pragma warning restore CS0618 // Type or member is obsolete
+            textPaint.AntiAlias=true;
+            textPaint.TextAlign=Paint.Align.Center;
+            hintPaint = new Paint();
+            hintPaint.TextSize=hintNormalSize;
+            hintPaint.AntiAlias=true;
+#pragma warning disable CS0618 // Type or member is obsolete
+            hintPaint.Color = Resources.GetColor(underlineColor);
+#pragma warning restore CS0618 // Type or member is obsolete
+
+        }
+        private void initAnimator()
+        {
+            reductionAnimator = ValueAnimator.OfFloat(0, underlineReduction);
+            reductionAnimator.SetDuration(animationDuration);
+            reductionAnimator.AddUpdateListener(new ReductionAnimatorListener());
+            reductionAnimator.SetInterpolator(new AccelerateDecelerateInterpolator());
+            hintSizeAnimator = ValueAnimator.OfFloat(hintNormalSize, hintSmallSize);
+            hintSizeAnimator.SetDuration(animationDuration);
+            hintSizeAnimator.AddUpdateListener(new HintSizeAnimatorListener());
+            hintSizeAnimator.SetInterpolator(new AccelerateDecelerateInterpolator());
+            hintYAnimator = ValueAnimator.OfFloat(0, hintMarginBottom);
+            hintYAnimator.SetDuration(animationDuration);
+            hintYAnimator.AddUpdateListener(new HintYAnimatorListener());
+            hintYAnimator.SetInterpolator(new AccelerateDecelerateInterpolator());
+        }
+        private void initViewOptions()
+        {
+            Focusable = true;
+            FocusableInTouchMode = true;
+        }
+        protected override void OnFocusChanged(bool gainFocus, [GeneratedEnum] FocusSearchDirection direction, Rect previouslyFocusedRect)
+        {
+            base.OnFocusChanged(gainFocus, direction, previouslyFocusedRect);
+            if (!gainFocus && characters.ToArray().Length == 0)
+            {
+                reverseAnimation();
+            }
+        }
+        protected override void OnSizeChanged(int w, int h, int oldw, int oldh)
+        { 
+            base.OnSizeChanged( (int)((underlineWidth + underlineReduction) * DEFAULT_CODES), (int)viewHeight, oldw, oldh);
+            height = h;
+            initUnderline();
+        }
+        protected override void OnMeasure(int widthMeasureSpec, int heightMeasureSpec)
+        {
+            base.OnMeasure(widthMeasureSpec, heightMeasureSpec);
+            SetMeasuredDimension((int)((underlineWidth + underlineReduction) * DEFAULT_CODES), (int)viewHeight);
+            
+        }
+        private void initUnderline()
+        {
+            for (int i = 0; i < underlineAmount; i++)
+            {
+                underlines[i] = createPath(i, underlineWidth);
+            }
+        }
+        private Underline createPath(int position, float sectionWidth)
+        {
+            float fromX = sectionWidth * (float)position;
+            return new Underline(fromX, height, fromX + sectionWidth, height);
+        }
+        private void showKeyboard()
+        {
+            InputMethodManager inputmethodmanager = (InputMethodManager)Context.GetSystemService(Context.InputMethodService);
+            inputmethodmanager.ShowSoftInput(this, (ShowFlags)InputMethodResults.UnchangedShown);
+            inputmethodmanager.ViewClicked(this);
+        }
+        private void hideKeyBoard()
+        {
+            InputMethodManager imm = (InputMethodManager)Context.GetSystemService(Context.InputMethodService);
+            //Input Method manager Doesnt have UnchangedShown
+            imm.HideSoftInputFromWindow(WindowToken,(HideSoftInputFlags)InputMethodResults.UnchangedShown);
+        }
+        public void setInputType(int inputType)
+        {
+            mInputType = inputType;
+        }
+        public override IInputConnection OnCreateInputConnection(EditorInfo outAttrs)
+        {
+            outAttrs.ActionLabel = null;
+            outAttrs.InputType = (Android.Text.InputTypes)mInputType;
+            outAttrs.ImeOptions = (ImeFlags) ImeAction.Done;
+            return new BaseInputConnection(this, true);
+        }
+        public override bool OnCheckIsTextEditor()
+        {
+            return true;
+        }
+        private void startAnimation()
+        {
+            reductionAnimator.Start();
+            hintSizeAnimator.Start();
+            hintYAnimator.Start();
+            underlined = false;
+        }
+        private void reverseAnimation()
+        {
+            reductionAnimator.Reverse();
+            hintSizeAnimator.Reverse();
+            hintYAnimator.Reverse();
+            underlined = true;
+        }
+        public override bool OnKeyDown([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            if (keyCode == Keycode.Del && characters.ToArray().Length != 0)
+               // if (keyCode == KeyEvent.KEYCODE_DEL && characters.size() != 0)
+                {
+                characters.Pop();
+
+                    }
+
+            return base.OnKeyDown(keyCode, e);
+        }
+        public override bool OnKeyUp([GeneratedEnum] Keycode keyCode, KeyEvent e)
+        {
+            string text = KeyEvent.KeyCodeToString(keyCode);
+            return true;
+
+        }
+      
+        public void setCode(string code) {
+            if (underlined)
+            {
+                startAnimation();
+            }
+            Handler handler = new Handler();
+            handler.PostDelayed(
+                delegate {
+                    StringBuilder stringBuilder;
+                    while (characters.ToArray().Length >= code.ToCharArray().Length && characters.ToArray().Length != 0)
+                    {
+                        characters.Pop();
+                    }
+                    foreach (char c in code.ToCharArray())
+                    {
+                        stringBuilder = new StringBuilder(KEYCODE);
+                        stringBuilder.Append(c);
+                        //inputText(string.CompareOrdinal(stringBuilder));
+                    }
+                    if (characters.ToArray().Length==0)
+                    {
+                        hideKeyBoard();
+                        reverseAnimation();
+                    }
+                } , animationDuration);
+
+        }
+        private bool inputText(string text)
+         {
+             Matcher matcher = KEYCODE_PATTERN.Matcher(text);
+             if (matcher.Matches()) {
+                chracter =(Character)(matcher.Group(1).ToCharArray().ElementAt(0).ToString());
+                 characters.Push(chracter);
+                 if (characters.getMaxSize() >= underlineAmount)
+                 {
+                     if (listener != null)
+                     {
+                         listener.onCodeReady(getCode());
+                     }
+                 }
+                 return true;
+             }
+             else
+             {
+                 return false;
+             }
+         }
+        public override bool OnTouchEvent(MotionEvent motionevent)
+        {
+            if (motionevent.Action == 0)
+            {
+                RequestFocus();
+                if (underlined)
+                {
+                    startAnimation();
+                }
+
+                showKeyboard();
+            }
+            return base.OnTouchEvent(motionevent);
+        }
+        protected override void OnDraw(Canvas canvas)
+        {
+            for (int i = 0; i < underlines.Length; i++)
+            {
+                Underline sectionpath = underlines[i];
+                float fromX = sectionpath.getFromX() + reduction;
+                float fromY = sectionpath.getFromY();
+                float toX = sectionpath.getToX() - reduction;
+                float toY = sectionpath.getToY();
+                drawSection(i, fromX, fromY, toX, toY, canvas);
+                if (characters.ToArray().Length > i && characters.ToArray().Length != 0)
+                {
+
+                    drawCharacter(fromX, toX, characters.ElementAt(0), canvas);
+                }
+            }
+
+            if (hintText != null)
+            {
+                drawHint(canvas);
+            }
+
+            Invalidate();
+        }
+        private void drawSection(int position, float fromX, float fromY, float toX, float toY, Canvas canvas)
+        {
+            Paint paint = underlinePaint;
+            if (position == characters.getMaxSize() && !underlined)
+            {
+                paint = underlineSelectedPaint;
+            }
+            canvas.DrawLine(fromX, fromY, toX, toY, paint);
+        }
+        private void drawCharacter(float fromX, float toX, Character character, Canvas canvas)
+        {
+            float actualWidth = toX - fromX;
+            float centerWidth = actualWidth / 2;
+            float centerX = fromX + centerWidth;
+            canvas.DrawText(character.ToString(), centerX, height - textMarginBottom, textPaint);
+        }
+        private void drawHint(Canvas canvas)
+        {
+            canvas.DrawText(hintText, hintX, height - textMarginBottom - hintActualMarginBottom, hintPaint);
+        }
+        public Character[] getCode()
+        {
+            Character[] ar = new Character [100];
+            ar = characters.ToArray();
+            return ar ;
+
+
+        }
+        private class ReductionAnimatorListener : Java.Lang.Object, ValueAnimator.IAnimatorUpdateListener
+           {
+               private float reduction;
+
+            
+
+            public void OnAnimationUpdate(ValueAnimator animation)
+            {
+                //Take String Argument
+                float value = ((Float)animation.GetAnimatedValue("")).FloatValue();
+                reduction = value;
+            }
+
+          
+       }
+        //End Of The First one 
+        private class HintYAnimatorListener : Java.Lang.Object, ValueAnimator.IAnimatorUpdateListener
+           {
+            private float hintActualMarginBottom;
+
+            public void OnAnimationUpdate(ValueAnimator animation)
+               {
+                    hintActualMarginBottom = (float) animation.GetAnimatedValue("");
+               }
+           }
+        //End of The Second
+       private class HintSizeAnimatorListener : Java.Lang.Object, ValueAnimator.IAnimatorUpdateListener
+        {
+            public void OnAnimationUpdate(ValueAnimator animation)
+            {
+                float size = (float) animation.GetAnimatedValue("");
+               // hintPaint.Set(size);
+            }
+        }
+        // override Animationupdae //End of the 3 one .
+
+    }
+    }
---
+++ ClassLibrary1/Data/FixedStack.cs
@@ -0,0 +1,30 @@
+﻿using Java.Lang;
+using System.Collections.Generic;
+using System;
+
+
+namespace ClassLibrary1 { 
+public class FixedStack<T> : Stack<T>
+{
+    int maxSize;
+
+
+    public int getMaxSize()
+    {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize)
+    {
+        this.maxSize = maxSize;
+    }
+
+
+
+
+
+
+}
+
+}
+}
